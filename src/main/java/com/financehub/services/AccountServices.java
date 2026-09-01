e16f9975cee168304dc087d1cb8d17ee877155ea
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-09-01 04:18:07 -0300

    finalizando services

--- src/main/java/com/financehub/services/AccountServices.java
+++ src/main/java/com/financehub/services/AccountServices.java
@@ -1,5 +1,55 @@
 package com.financehub.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.financehub.domain.Account;
+import com.financehub.exceptions.ResourceNotFoundException;
+import com.financehub.repositories.AccountRepositories;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
 public class AccountServices {
 
+	@Autowired
+	private AccountRepositories accountRepo;
+	
+	public List<Account> findAll(){
+		return accountRepo.findAll();
+	}
+	public Account findByid(String id) {
+		Optional<Account> obj = accountRepo.findById(id);//averigua se tem ou nao tem.
+		return obj.orElseThrow(() -> new RuntimeException(id));//retorne obj, ou entao lançe uma exceção
+	}
+	public Account insert(Account account) {
+		return accountRepo.save(account);
+	}
+	public Account update(String id, Account account) {
+		try {
+			Account entity = accountRepo.getReferenceById(id);
+			updateData(entity,account);
+			return accountRepo.save(entity);
+		}catch(EntityNotFoundException e) { 
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	public void updateData(Account entity, Account account) {
+		entity.setStatus(account.getStatus());
+		entity.setType(account.getType());
+		entity.setCreatedAt(account.getCreatedAt());
+		entity.setNumberAccount(account.getNumberAccount());
+	}
+	public void delete(String id) {
+		try {
+			accountRepo.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
 }
--- src/main/java/com/financehub/services/TransantionServices.java
+++ src/main/java/com/financehub/services/TransantionServices.java
@@ -1,5 +1,30 @@
 package com.financehub.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.financehub.domain.Transantion;
+import com.financehub.repositories.TransantionRepositories;
+
+@Service
 public class TransantionServices {
+	
+	@Autowired
+	private TransantionRepositories transantionRepo;
+	
+	public List<Transantion> findAll(){
+		return transantionRepo.findAll();
+	}
+	
+	public Transantion findById(String id) {
+		Optional<Transantion> obj = transantionRepo.findById(id);
+		return obj.orElseThrow(() -> new RuntimeException(id));
+	}
+	public Transantion insert(Transantion transantion) {
+		return transantionRepo.save(transantion);
+	}
 
 }
